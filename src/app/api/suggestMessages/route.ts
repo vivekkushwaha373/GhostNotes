af8b58e2340d7467f77e05221da8b6c257d85b37
Vivek kushwaha <109839269+vivekkushwaha373@users.noreply.github.com>
2025-10-04 15:11:18 +0530

    Update route.ts
    changed gemini model

--- src/app/api/suggestMessages/route.ts
+++ src/app/api/suggestMessages/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 
         // const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: process.env.GEMINI_MODEL});
 
         const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience.For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simple thing that makes you happy?'. Ensure the questions are intriguing, be it personal professional or friendly";
 
@@ -32,3 +32,4 @@ export async function POST(req: Request) {
         }, { status: 500 })
     }
 }
+
